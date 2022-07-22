6fefa64e838ef7a0ac4f536b1707474bb43c6be4
JohnNotzold <profjohn.notzold@gmail.com>
2022-07-22 10:02:52 -0300

    armazenando dados e interagindo com o prompt

---
+++ aula11/ex001.js
@@ -0,0 +1,8 @@
+var vel = 6
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+    if (vel > 60){
+        console.log('MULTADO')
+    }else{
+        console.log('Passa Passarinho!')
+    }
+console.log('Dirija sempre usando cinto de segurança')
