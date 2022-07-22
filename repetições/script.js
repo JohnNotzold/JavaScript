59c998dbaaee68a28064fb7afe1ab754ab27b1be
JohnNotzold <profjohn.notzold@gmail.com>
2022-07-22 13:56:33 -0300

    condições e afins

---
+++ repetições/script.js
@@ -0,0 +1,14 @@
+var n = 10
+var i = 1
+
+ while(i<=n){      
+       
+        if(i%2==0){
+            console.log(`o número ${i} é par`)
+        }else{
+            console.log(`o número ${i} é ímpar`)
+        }
+        i++
+    }
+
+
